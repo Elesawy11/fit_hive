7cc5fcfdff2d0d4d42035cf78ceffd44b4fd8c9c
elesawy2 <elesawy928@gmail.com>
2024-11-26 01:43:08 +0200

    create custom text button

---
+++ lib/features/welcome/presentation/widgets/custom_text_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/color_helper.dart';
+import '../../../../core/theming/font_styles.dart';
+
+class CustomTextbutton extends StatelessWidget {
+  const CustomTextbutton({
+    super.key, this.onPressed,
+  });
+final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Container(
+        height: 60.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          gradient: LinearGradient(colors: [
+            ColorHelper.color9DC(),
+            ColorHelper.color92A(),
+          ]),
+        ),
+        child: Center(
+          child: Text(
+            'Get Started',
+            style: FontStyles.font16Bold().copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
