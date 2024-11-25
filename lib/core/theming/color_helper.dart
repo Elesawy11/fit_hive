08043aee42f7941a761827e4733a9e82ea76ee9e
elesawy2 <elesawy928@gmail.com>
2024-11-26 00:25:05 +0200

    add helper method and sharing theme

---
+++ lib/core/theming/color_helper.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorHelper {
+  static Color color92A() => const Color(0xff92A3FD);
+  static Color color7B6() => const Color(0xff7B6F72);
+}
---
+++ lib/core/theming/font_styles.dart
@@ -0,0 +1,11 @@
+import 'package:fit_hive/core/theming/font_weight_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class FontStyles {
+  static TextStyle font36SemiBold() =>
+      TextStyle(fontSize: 36.sp, fontWeight: FontWeightHelper.semiBold);
+      
+      static TextStyle font18Regular() =>
+      TextStyle(fontSize: 18.sp, fontWeight: FontWeightHelper.regular);
+}
