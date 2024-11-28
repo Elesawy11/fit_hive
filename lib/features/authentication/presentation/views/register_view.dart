ccaa2acc2b6d62f3eadd1ca7a71f11a06d8878e2
elesawy2 <elesawy928@gmail.com>
2024-11-28 04:03:12 +0200

    create row of dividers and text widget

--- lib/features/authentication/presentation/views/register_view.dart
+++ lib/features/authentication/presentation/views/register_view.dart
@@ -1,9 +1,11 @@
 import 'package:fit_hive/core/helpers/spacers.dart';
 import 'package:fit_hive/core/theming/color_helper.dart';
+import 'package:fit_hive/core/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/theming/font_styles.dart';
 import 'widgets/column_of_text_form_field.dart';
+import 'widgets/row_of_dividers_and_text.dart';
 import 'widgets/row_of_icon_button_and_text.dart';
 
 class RegisterView extends StatelessWidget {
@@ -33,6 +35,12 @@ class RegisterView extends StatelessWidget {
               const ColumnOfTextFormField(),
               const RowOfIconButtonAndText(),
               verticalSpace(40),
+              CustomTextbutton(
+                text: 'Register',
+                onPressed: () {},
+              ),
+              verticalSpace(24),
+              const RowOfDividersAndText(),
               const Spacer(),
             ],
           ),
@@ -41,4 +49,3 @@ class RegisterView extends StatelessWidget {
     );
   }
 }
-
