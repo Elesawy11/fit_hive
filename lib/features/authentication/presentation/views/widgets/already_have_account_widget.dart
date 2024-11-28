6e20e7d6dabab68abc68bf508f2205bf2fe970ce
elesawy2 <elesawy928@gmail.com>
2024-11-28 06:57:39 +0200

    refactor already have account widget

--- lib/features/authentication/presentation/views/widgets/already_have_account_widget.dart
+++ lib/features/authentication/presentation/views/widgets/already_have_account_widget.dart
@@ -5,23 +5,27 @@ import '../../../../../core/theming/font_styles.dart';
 class AlreadyHaveAccountWidget extends StatelessWidget {
   const AlreadyHaveAccountWidget({
     super.key,
+    required this.text,
+    required this.operat,
+    this.onTap,
   });
-
+  final String text, operat;
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          'Already have an account?',
+          text,
           style: FontStyles.font14Regular().copyWith(
             color: ColorHelper.mainColor(),
           ),
         ),
         GestureDetector(
-          onTap: () {},
+          onTap: onTap,
           child: Text(
-            ' Login',
+            operat,
             style: FontStyles.font14Regular().copyWith(
               color: ColorHelper.color844(),
             ),
