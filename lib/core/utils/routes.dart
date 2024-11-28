eec45e5d148c1db637131e7353b59d4f074476cb
elesawy2 <elesawy928@gmail.com>
2024-11-28 07:13:50 +0200

    finish login view

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -1,3 +1,4 @@
+import 'package:fit_hive/features/authentication/presentation/views/login_view.dart';
 import 'package:fit_hive/features/authentication/presentation/views/register_view.dart';
 import 'package:fit_hive/features/onboarding/presentation/views/onboarding.dart';
 import 'package:fit_hive/features/welcome/presentation/views/welcome_view.dart';
@@ -7,10 +8,12 @@ List<GetPage<dynamic>> getPageList = [
   GetPage(name: Routes.welcomeView, page: () => const WelcomeView()),
   GetPage(name: Routes.onboardingView, page: () => const OnboardingView()),
   GetPage(name: Routes.registerView, page: () => const RegisterView()),
+  GetPage(name: Routes.loginView, page: () => const LoginView()),
 ];
 
 abstract class Routes {
   static const String welcomeView = '/';
   static const String onboardingView = '/onboarding';
   static const String registerView = '/registerView';
+  static const String loginView = '/loginView';
 }
