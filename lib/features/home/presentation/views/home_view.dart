018067eb251e525e67a678ac2668c7b899285729
elesawy2 <elesawy928@gmail.com>
2024-12-02 01:40:47 +0200

    bottom navigaation bar testing

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: (value) {
+            currentIndex = value;
+            setState(() {});
+          },
+          currentIndex: currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo_camera_outlined),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'home',
+            ),
+          ]),
+      body: [
+        Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          body: const Center(
+              child: Text(
+            'Home',
+            style: TextStyle(fontSize: 36),
+          )),
+        ),
+        const Center(
+            child: Text(
+          'Photo',
+          style: TextStyle(fontSize: 36),
+        )),
+        const Center(
+            child: Text(
+          'Profile',
+          style: TextStyle(fontSize: 36),
+        )),
+      ].elementAt(currentIndex),
+    );
+  }
+
+  BottomNavigationBarItem bootmNavigationBarItem({
+    required Widget icon,
+    required String label,
+    // required int index,
+  }) {
+    return BottomNavigationBarItem(
+      icon: icon,
+      label: label,
     );
   }
 }
