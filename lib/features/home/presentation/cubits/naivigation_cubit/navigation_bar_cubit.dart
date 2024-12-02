ef2cb76f15f4e15c4c501cc0639d5ba042d4fbd5
elesawy2 <elesawy928@gmail.com>
2024-12-02 02:45:49 +0200

    rebuild NavigationBarCubit and trigger it on home view

--- lib/features/home/presentation/cubits/naivigation_cubit/navigation_bar_cubit.dart
+++ lib/features/home/presentation/cubits/naivigation_cubit/navigation_bar_cubit.dart
@@ -1,14 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
-part 'navigation_bar_state.dart';
+class NavigationBarCubit extends Cubit<int> {
+  NavigationBarCubit() : super(0);
 
-class NavigationBarCubit extends Cubit<NavigationBarState> {
-  NavigationBarCubit() : super(NavigationBarInitial());
-  int currentIndex = 0;
-
-  void navigateToIndex({required int index}) {
-    currentIndex = index;
-    emit(ChangeIndex());
-  }
+  void navigateToIndex({required int index}) => emit(index);
 }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -2,70 +2,66 @@ import 'package:fit_hive/features/home/presentation/cubits/naivigation_cubit/nav
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => NavigationBarCubit(),
-      child: BlocBuilder<NavigationBarCubit, NavigationBarState>(
+      child: Scaffold(bottomNavigationBar: BlocBuilder<NavigationBarCubit, int>(
         builder: (context, state) {
-          return Scaffold(
-            bottomNavigationBar: BottomNavigationBar(
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                onTap: (index) {
-                  context
-                      .read<NavigationBarCubit>()
-                      .navigateToIndex(index: index);
-                },
-                currentIndex: context.read<NavigationBarCubit>().currentIndex,
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home_outlined),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.photo_camera_outlined),
-                    label: 'home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person_outline),
-                    label: 'home',
-                  ),
-                ]),
-            body: [
-              Scaffold(
-                floatingActionButton: FloatingActionButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.add),
-                ),
-                body: const Center(
-                    child: Text(
-                  'Home',
-                  style: TextStyle(fontSize: 36),
-                )),
+          final currentIndex = state;
+          return BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            onTap: (index) {
+              context.read<NavigationBarCubit>().navigateToIndex(index: index);
+            },
+            currentIndex: currentIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'home',
               ),
-              const Center(
-                  child: Text(
-                'Photo',
-                style: TextStyle(fontSize: 36),
-              )),
-              const Center(
-                  child: Text(
-                'Profile',
-                style: TextStyle(fontSize: 36),
-              )),
-            ].elementAt(context.read<NavigationBarCubit>().currentIndex),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.photo_camera_outlined),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: 'home',
+              ),
+            ],
           );
         },
-      ),
+      ), body: BlocBuilder<NavigationBarCubit, int>(
+        builder: (context, state) {
+          return [
+            Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+              body: const Center(
+                  child: Text(
+                'Home',
+                style: TextStyle(fontSize: 36),
+              )),
+            ),
+            const Center(
+                child: Text(
+              'Photo',
+              style: TextStyle(fontSize: 36),
+            )),
+            const Center(
+                child: Text(
+              'Profile',
+              style: TextStyle(fontSize: 36),
+            )),
+          ].elementAt(state);
+        },
+      )),
     );
   }
 
