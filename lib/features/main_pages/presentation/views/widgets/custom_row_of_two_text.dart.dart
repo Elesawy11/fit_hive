09fbbbf117765a04a8997f989557cc54b3713da3
elesawy2 <elesawy928@gmail.com>
2024-12-04 00:46:29 +0200

    change customLatestWidget to Custom rowOfTwoText

--- lib/features/main_pages/presentation/views/widgets/custom_row_of_two_text.dart.dart
+++ lib/features/main_pages/presentation/views/widgets/custom_row_of_two_text.dart.dart
@@ -2,10 +2,11 @@ import 'package:fit_hive/core/theming/color_helper.dart';
 import 'package:fit_hive/core/theming/font_styles.dart';
 import 'package:flutter/material.dart';
 
-class CustomLatestWorkWidget extends StatelessWidget {
-  const CustomLatestWorkWidget({
-    super.key,
+class CustomRowOfTwoText extends StatelessWidget {
+  const CustomRowOfTwoText({
+    super.key, required this.boldText, required this.normalText,
   });
+  final String boldText,normalText;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +14,13 @@ class CustomLatestWorkWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          'Latest workout',
+          boldText,
           style: FontStyles.font16Medium().copyWith(
             color: ColorHelper.mainColor(),
           ),
         ),
         Text(
-          'see more',
+          normalText,
           style: FontStyles.font12Regular().copyWith(
             color: ColorHelper.secondaryColor(),
           ),
