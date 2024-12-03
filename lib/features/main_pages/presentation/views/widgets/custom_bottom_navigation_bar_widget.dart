2f4dfdf3488f5f71475926373f0c2b946552af3f
elesawy2 <elesawy928@gmail.com>
2024-12-03 02:54:51 +0200

    ediat navigation bar

--- lib/features/main_pages/presentation/views/widgets/custom_bottom_navigation_bar_widget.dart
+++ lib/features/main_pages/presentation/views/widgets/custom_bottom_navigation_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../cubits/naivigation_cubit/navigation_bar_cubit.dart';
 
 class CustomBottomNavigationBarWidget extends StatelessWidget {
@@ -13,6 +14,8 @@ class CustomBottomNavigationBarWidget extends StatelessWidget {
       builder: (context, state) {
         final currentIndex = state;
         return BottomNavigationBar(
+          iconSize: 28.r,
+          backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           onTap: (index) {
@@ -20,15 +23,24 @@ class CustomBottomNavigationBarWidget extends StatelessWidget {
           },
           currentIndex: currentIndex,
           items: [
-            bootmNavigationBarItem(
+            bottmNavigationBarItem(
+              currentIndex: currentIndex,
+              selectedIcon: const Icon(Icons.home),
+              index: 0,
               icon: const Icon(Icons.home_outlined),
               label: 'Home',
             ),
-            bootmNavigationBarItem(
+            bottmNavigationBarItem(
+              currentIndex: currentIndex,
+              index: 1,
+              selectedIcon: const Icon(Icons.photo_camera),
               icon: const Icon(Icons.photo_camera_outlined),
               label: 'Progress Tracker',
             ),
-            bootmNavigationBarItem(
+            bottmNavigationBarItem(
+              currentIndex: currentIndex,
+              index: 2,
+              selectedIcon: const Icon(Icons.person),
               icon: const Icon(Icons.person_outline),
               label: 'Profile',
             ),
@@ -38,13 +50,15 @@ class CustomBottomNavigationBarWidget extends StatelessWidget {
     );
   }
 
-  BottomNavigationBarItem bootmNavigationBarItem({
+  BottomNavigationBarItem bottmNavigationBarItem({
     required Widget icon,
+    required Widget selectedIcon,
     required String label,
-    // required int index,
+    required int index,
+    required int currentIndex,
   }) {
     return BottomNavigationBarItem(
-      icon: icon,
+      icon: currentIndex == index ? selectedIcon : icon,
       label: label,
     );
   }
