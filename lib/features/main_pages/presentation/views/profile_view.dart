60e24191737f75abab5a7260eb337a0fb0d02de8
elesawy2 <elesawy928@gmail.com>
2024-12-04 01:55:36 +0200

    create structure of profile view

---
+++ lib/features/main_pages/presentation/views/profile_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: const Column(),
+    );
+  }
+}
